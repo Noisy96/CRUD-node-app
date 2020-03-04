e4b73e93fc946b26d98073b328f8621c052ddc0f
Rahmouni Abdelhak <rahmouni.abdelhak.1@gmail.com>
2020-03-04 21:50:00 +0100

    add controllers

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require('express'); 
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const stuffRoutes = require('./routes/stuff');
+
 const app = express();
 
+// Reading the database connection credentials from a file saved locally instead of hardcoding it
 try {
     var dbCredentials = fs.readFileSync('dbCredentials.txt', 'utf8');
 } catch (e) {
     console.log('Error:', e.stack);
 }
 
+// Establishes database connection
 mongoose.connect('mongodb+srv://' + dbCredentials + '@cluster0-q3g82.mongodb.net/test?retryWrites=true&w=majority').then(() => {
     console.log('Successfully connected to MongoDB Atlas!');
 })
@@ -19,6 +22,7 @@ mongoose.connect('mongodb+srv://' + dbCredentials + '@cluster0-q3g82.mongodb.net
         console.error(error);
     });
 
+// Adding headers to avoid Cross Origin Resource Sharing errors
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, C-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -26,8 +30,10 @@ app.use((req, res, next) => {
     next();
 });
 
+// Parsing json objects transmitted to be read
 app.use(bodyParser.json());
 
+// Hooking the routes
 app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
