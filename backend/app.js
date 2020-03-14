ac7b841429224798c78600b1068968e70ef0318e
Rahmouni Abdelhak <rahmouni.abdelhak.1@gmail.com>
2020-03-14 17:44:42 +0100

    file uploading

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const fs = require('fs');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 // Hooking the routes
+app.use('/images', express.static(path.join(__dirname,'images')));
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const mutler = require('../middleware/mutler-config');
 
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, mutler, stuffCtrl.createThing);
 router.get('/', auth, stuffCtrl.getAllThings);
 router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.updateOneThing);
+router.put('/:id', auth, mutler, stuffCtrl.updateOneThing);
 router.delete('/:id', auth, stuffCtrl.deleteOneThing);
 
 module.exports = router;
